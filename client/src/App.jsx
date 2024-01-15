c96f5de1e5a214c6bd51c0f21fc29e57ac3702eb
Kennedy Vandel <34487378+vandelk@users.noreply.github.com>
2024-01-14 19:29:49 -0800

    switched alcohol route to ingredients, still working with API to get data that I want

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {Routes, Route, Link, useNavigate, Navigate} from 'react-router-dom'
-import Dashboard from './components/dashboard'
+import Dashboard from './components/Dashboard'
 import Ingredient from './components/Ingredient'
 
 import './App.css'
@@ -11,14 +11,14 @@ function App() {
       <div>
         <h1>Cocktail Recipes</h1>
         <Link to="/categories">Categories</Link>
-        <Link to="/alcohol">Alcohols</Link>
+        <Link to="/ingredients">Alcohols</Link>
 
         <Routes>
           <Route path="/" element={<Navigate to='/categories'/>}/>
           {/* DASHBOARD*/}
           <Route path="/categories" element={<Dashboard/>}/>
 
-          <Route path="/alcohol" element={<Ingredient/>}/>
+          <Route path="/ingredients" element={<Ingredient/>}/>
         </Routes>
       </div>
     </>
--- client/src/components/Ingredient.jsx
+++ client/src/components/Ingredient.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Ingredient = () => {
+  const [ingredient, setIngredient] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+      .then(res => {
+        setIngredient(res.data.drinks)
+      })
+      .catch(err => console.log(err))
+  }, []);
+
   return (
-    <div>Ingredient</div>
+    <div>
+      <h3>Ingredients</h3>
+      {ingredient.map((eachIngred) => {
+        return (
+          <div key={eachIngred.strIngredient1}>
+            <h3>{eachIngred.strIngredient1}</h3>
+          </div>
+        )
+      })}
+    </div>
+
   )
 }
 
